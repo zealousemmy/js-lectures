1811d6d78c89b7a602aac3ae065bc79b19a9038c
Emmyzealous <emmygazzillion123@gail.com>
2022-11-21 19:26:24 +0100

    resolved_on_stanjs

--- js/stanleyjs/index.js
+++ js/stanleyjs/index.js
@@ -5,24 +5,25 @@ let userAge = document.getElementById("uAge");
 let userAddress = document.getElementById("uAddress");
 let userPassword = document.getElementById("uPassword");
 let submitButton = document.querySelector("#submit");
+let h1Text = document.getElementById("text");
 
-function userInfo(){
-    let fName = fName.value;
-    let uName = uName.value;
-    let uAge = uAge.value;
-    let uAddress = uAddress.value;
-    let uPassword = uPassword.value;
+function userInfo() {
+  let fName = fullName.value;
+  let uName = userName.value;
+  let uAge = userAge.value;
+  let uAddress = userAddress.value;
+  let uPassword = userPassword.value;
 
-    h1Text.innerHTML = `Welcome, let's create magic ${fName}`;
+  h1Text.innerHTML = `Welcome, let's create magic ${fName}`;
 }
 
 submitButton.addEventListener("click", userInfo);
 
-function changeColor(){
-    if(display.className == "display"){
-        display.classList.add("bgGreen");
-    }else{
-        display.classList.remove("bgGreen");
-    }
-    console.log("clicked me" + display);
+function changeColor() {
+  if (display.className == "display") {
+    display.classList.add("bgGreen");
+  } else {
+    display.classList.remove("bgGreen");
+  }
+  console.log("clicked me" + display);
 }
